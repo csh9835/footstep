661e33f6826ce3a108b934c6fc38a97730e098ae
Choi_Seung_Ho <aabb5389@naver.com>
2022-11-26 19:37:10 +0900

    세부구현 필요

--- footstep/models.py
+++ footstep/models.py
@@ -1,18 +1,23 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+class SidebarContent(models.Model):
+    category_sub = models.CharField(max_length=200)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.category_sub
+
 class Post(models.Model):
     subject = models.CharField(max_length=200)
     content = models.TextField()
     create_date = models.DateTimeField()
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+    category = models.ForeignKey(SidebarContent, on_delete=models.PROTECT)
 
     def __str__(self):
         return self.subject
 
-class Category(models.Model):
-    category_sub = models.CharField(max_length=200)
-    posts = models.ForeignKey(Post, on_delete=models.DO_NOTHING)
 
 #class Comment(models.Model):
     #post = models.ForeignKey(Post, on_delete=models.CASCADE)
