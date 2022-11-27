21705c2568f6a77687fc57cf500c0ce03867a916
Choi_Seung_Ho <aabb5389@naver.com>
2022-11-27 18:35:03 +0900

    +변수 수정

--- footstep/urls.py
+++ footstep/urls.py
@@ -7,5 +7,6 @@ app_name = 'footstep'
 urlpatterns = [
     path('', views.index, name='index'),
     path('<str:username>/', views.personal, name='personal'),
-    path('<str:username>/<str:category_sub>/', views.category, name='category'),
+    path('<str:username>/category=<str:category_sub>/', views.category, name='category'),
+    path('<str:username>/post=<str:subject>/', views.post, name='post'),
 ]
--- footstep/views.py
+++ footstep/views.py
@@ -7,13 +7,19 @@ def index(request):
     return render(request, 'footstep/main.html', context)
 
 def personal(request, username):
-    owneruser = get_object_or_404(User, username=username)
-    posts = owneruser.post_set.order_by('-create_date')
-    context = {'owneruser':owneruser, 'posts':posts}
+    owner = get_object_or_404(User, username=username)
+    posts = owner.post_set.order_by('-create_date')
+    context = {'owner':owner, 'posts':posts}
     return render(request, 'footstep/personal.html', context)
 
 def category(request, username, category_sub):
-    owneruser = get_object_or_404(User, username=username)
-    category = get_object_or_404(owneruser.sidebarcontent_set, category_sub=category_sub)
-    context = {'owneruser':owneruser, 'category':category}
+    owner = get_object_or_404(User, username=username)
+    category = get_object_or_404(owner.sidebarcontent_set, category_sub=category_sub)
+    context = {'owner':owner, 'category':category}
     return render(request, 'footstep/category.html', context)
+
+def post(request, username, subject):
+    owner = get_object_or_404(User, username=username)
+    post = get_object_or_404(owner.post_set, subject=subject)
+    context = {'owner':owner, 'post':post}
+    return render(request, 'footstep/post.html', context)
